f83c4a40b7a87c8cd1d2fc48cb5c694b5e5abc35
Pradip-web-Bariflolabs <154691184+Pradip-web-Bariflolabs@users.noreply.github.com>
2023-12-27 23:38:50 +0530

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from  .models import User
-
-@admin.register(User)
-class User(admin.ModelAdmin):
-    list_display = ['Name','Email','Mobno','password','Adhaar','token','user_category']
-   
